554279b7983b78a48b33ed9efb11c8256a2ebf42
Rohithram R <rohithram.r@flutura.com>
2018-06-29 15:55:03 +0530

    changed acknowledegement json to the latest, added some test cases for som_knn

--- rohithram/anomaly_detectors/utils/make_ackg_json.py
+++ rohithram/anomaly_detectors/utils/make_ackg_json.py
@@ -24,9 +24,10 @@ def make_ack_json(anomaly_detectors):
     no_anom_response = {"code":"200","status" : "OK","message": "No Anomalies detected"}
     
     ack_json = lambda:{"header":'',"body":[]}
-    anom_per_asset  = lambda:{"asset": "<asset_serial_number>","anomalies":[],"anom_counts":{}}
+    anom_per_asset  = lambda:{"asset": "<asset_serial_number>","anomalies":[]}
     anom_per_metric = lambda:{ "name":"<TagName>","datapoints":[]}
-    
+    Datapoint_keys = ['from_timestamp','to_timestamp','anomaly_timestamp','anomaly_code']            
+
     ack_json1 = ack_json()
     
     if(anomaly_detectors[0].algo_type=='univariate'):
@@ -45,17 +46,17 @@ def make_ack_json(anomaly_detectors):
                 if(len(data[anomaly_detector.metric_name])!=0):
                     ack_json1['header'] = error_codes.error_codes['success']
                     anom_per_asset1['asset'] = anomaly_detector.assetno
-                    anom_per_asset1['anom_counts'][anomaly_detector.metric_name] = len(anom_indexes)
+#                     anom_per_asset1['anom_counts'][anomaly_detector.metric_name] = len(anom_indexes)
+                    if(len(anom_indexes)!=0):
+                        anom_per_metric1 = anom_per_metric()
+                        anom_per_metric1['name'] = anomaly_detector.metric_name
+                        anom_timestamps = data.index[anom_indexes]
                     
-                    anom_per_metric1 = anom_per_metric()
-                    anom_per_metric1['name'] = anomaly_detector.metric_name
-                    anom_timevalues = data.index[anom_indexes]
-                    anom_codes = [anomaly_detector.algo_code for i in range(len(anom_timevalues))]
-
-                    anom_timestamps = list(zip(anom_timevalues,anom_timevalues,anom_timevalues,anom_codes))
-                    anom_per_metric1['datapoints'] = [dict(zip(['from_timestamp','to_timestamp','anomaly_timestamp','anomaly_code'],anom_timestamp)) for anom_timestamp in anom_timestamps]                    
-
-                    anom_per_asset1['anomalies'].append(anom_per_metric1)
+                        anom_per_metric1['datapoints'] = [dict(zip(Datapoint_keys,
+                                                               [anom_timestamp,anom_timestamp,[anom_timestamp],
+                                                                anomaly_detector.algo_code]))
+                                                      for anom_timestamp in anom_timestamps]               
+                        anom_per_asset1['anomalies'].append(anom_per_metric1)
                 
                 else:
                     ack_json1['header'] = bad_response
@@ -80,15 +81,17 @@ def make_ack_json(anomaly_detectors):
 
                 for metric_name in metric_names:
                     
-                    anom_per_asset1['anom_counts'][metric_name] = len(anom_indexes)
+#                     anom_per_asset1['anom_counts'][metric_name] = len(anom_indexes)
 
                     anom_per_metric1 = anom_per_metric()
                     anom_per_metric1['name'] = metric_name
-                    anom_timevalues = data.index[anom_indexes]
-                    anom_codes = [anomaly_detector.algo_code for i in range(len(anom_timevalues))]
-                    anom_timestamps = list(zip(anom_timevalues,anom_timevalues,anom_timevalues,anom_codes))
+                    anom_timestamps = data.index[anom_indexes]
                     
-                    anom_per_metric1['datapoints'] = [dict(zip(['from_timestamp','to_timestamp','anomaly_timestamp','anomaly_code'],anom_timestamp)) for anom_timestamp in anom_timestamps]                    
+                    anom_per_metric1['datapoints'] = [dict(zip(Datapoint_keys,
+                                                               [anom_timestamp,anom_timestamp,[anom_timestamp],
+                                                                anomaly_detector.algo_code]))
+                                                      for anom_timestamp in anom_timestamps]                    
+
                     anom_per_asset1['anomalies'].append(anom_per_metric1)
 
                 ack_json1['body'].append(anom_per_asset1)
